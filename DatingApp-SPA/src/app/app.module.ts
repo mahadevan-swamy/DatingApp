818b6c87bf128952ed7a81f4f6e0e39e9363fa03
Mahadevan Swamy <mahadevan.swamy@cfm-us.com>
2020-02-15 02:07:58 -0500

    End of Module 6

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -9,10 +9,18 @@ import { AuthService } from "./_services/auth.service";
 import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from "./register/register.component";
 import { ErrorInterceptorProvider } from "./_services/error_interceptor";
+import { BsDropdownModule } from "ngx-bootstrap";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
+  ],
   providers: [AuthService, ErrorInterceptorProvider],
   bootstrap: [AppComponent]
 })
